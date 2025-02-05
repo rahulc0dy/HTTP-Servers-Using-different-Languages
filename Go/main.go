1545f4effa7583c7d60004e48b05eee8a3b5da32
rahulc0dy <rahulchakraborty337@gmail.com>
2025-02-05 16:55:00 +0530

    Add Go HTTP server implementation and Rust .gitignore file

---
+++ Go/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":8000", mux)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed")
+		os.Exit(0)
+	} else {
+		fmt.Printf("server error: %s\n", err)
+		os.Exit(1)
+	}
+}
